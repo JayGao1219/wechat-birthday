e67c0f694cccccff7913862d7620294900dc3959
jaygao1219 <jaygao1219@gmail.com>
2023-03-12 11:44:43 +0800

    [add] 游戏音效

--- pages/game/game.js
+++ pages/game/game.js
@@ -6,13 +6,13 @@ Page({
     classStyle: '',
     value:0,
     id:'default',
+    src:"../../audio/beat.mp3",
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.audioCtx = wx.createAudioContext('myAudio');
   },
   click() {
     this.setData({
@@ -20,6 +20,7 @@ Page({
       id:'font',
       value:this.data.value+1,
     });
+    this.audioCtx.play();
     setTimeout(() => {
       this.setData({
         classStyle: '',
